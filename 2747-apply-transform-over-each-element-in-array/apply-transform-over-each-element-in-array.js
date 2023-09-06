d36b18d6cebd1f3d5a38d17900036101213f198e
jagadeesh <46147427+jagadeesh944078@users.noreply.github.com>
2023-09-06 12:12:29 +0530

    Time: 54 ms (50.34%) | Memory: 41.9 MB (43.30%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -5,8 +5,18 @@
  */
 var map = function(arr, fn) {
     let transformedArray = [];
-    for(let i=0; i<arr.length;i++){
-        transformedArray[i] = fn(arr[i], i);
-    }
-     return transformedArray;
+    // arr.forEach((item,index) => {
+    //     transformedArray[index] = fn(item, index);
+    // });
+    //   return arr.reduce((transformedArray, element, index) => {
+    //       transformedArray[index] = fn(element, index);
+    //       return transformedArray;
+    //   },[])
+        return arr.map((element, index)=>
+            fn(element,index)
+        )
+    // for(let i=0; i<arr.length;i++){
+    //     transformedArray[i] = fn(arr[i], i);
+    // }
+    // return transformedArray;
 };
