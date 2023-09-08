a0002b2df312adc931a520c3ec602a49cc4e6838
jagadeesh <46147427+jagadeesh944078@users.noreply.github.com>
2023-09-08 15:47:08 +0530

    Time: 64 ms (63.44%) | Memory: 43.4 MB (36.16%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -3,19 +3,10 @@
  * @return {Function}
  */
 var compose = function(functions) {
-    if(functions.length===0) {
-        return function (x) {
-            return x;
-        }
-    }
-    else {
-         return function(x) {
-            var result = functions[functions.length - 1](x);
-            for(var i = functions.length-2; i>=0; i--) {
-                result = functions[i](result);
-            }
-            return result;
-         }
+	return function(x) {
+        return functions.reduceRight((acc,element) => {
+            return element(acc);
+        },x)
     }
 };
 
